ddde8310da52c9e3f7b77547eca7b06116c2730e
DanillS <104566082+DanillS@users.noreply.github.com>
2025-10-19 19:43:11 +0300

    Add files via upload

---
+++ data/doors.js
@@ -0,0 +1,62 @@
+export const doors = [
+  {
+    id: 1,
+    name: "Дверь классическая 'Венеция'",
+    price: 100,
+    material: "Массив дуба",
+    size: "2000×800 мм",
+    color: "Белый",
+    image: "/doors/door1.jpg",
+    description: "Классическая межкомнатная дверь из массива дуба"
+  },
+  {
+    id: 2,
+    name: "Дверь современная 'Милан'",
+    price: 8900,
+    material: "МДФ",
+    size: "2000×700 мм",
+    color: "Серый",
+    image: "/doors/door2.jpg",
+    description: "Современная дверь с матовым покрытием"
+  },
+  {
+    id: 3,
+    name: "Дверь раздвижная 'Токио'",
+    price: 15600,
+    material: "Стекло + алюминий",
+    size: "2200×900 мм",
+    color: "Черный",
+    image: "/doors/door3.jpg",
+    description: "Стильная раздвижная дверь-купе"
+  },
+  {
+    id: 4,
+    name: "Дверь входная 'Форт'",
+    price: 23400,
+    material: "Сталь",
+    size: "2050×860 мм",
+    color: "Коричневый",
+    image: "/doors/door4.jpg",
+    description: "Надежная стальная входная дверь"
+  },
+  {
+    id: 5,
+    name: "Дверь межкомнатная 'Флоренция'",
+    price: 11200,
+    material: "Шпон",
+    size: "2000×800 мм",
+    color: "Бежевый",
+    image: "/doors/door5.jpg",
+    description: "Элегантная дверь с шпонированным покрытием"
+  },
+  {
+    id: 6,
+    name: "Дверь стеклянная 'Нева'",
+    price: 18700,
+    material: "Закаленное стекло",
+    size: "2100×750 мм",
+    color: "Прозрачный",
+    image: "/doors/door6.jpg",
+    description: "Стеклянная дверь для современных интерьеров"
+  }
+];
